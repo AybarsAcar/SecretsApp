6bb512928701c71e02f074de5b302ec10f872b65
Aybars Acar <aybarsacar@gmail.com>
2020-05-26 19:52:08 +1000

    bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -39,17 +40,21 @@ app.get("/register", function(req, res){
 
 //creating a user account
 app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err){
+      if (err){
+        console.log(err);
+      } else{
+        res.render("secrets");
+      }
+    });
   });
-  newUser.save(function(err){
-    if (err){
-      console.log(err);
-    } else{
-      res.render("secrets");
-    }
-  });
+
 });
 
 //logging in
@@ -62,9 +67,13 @@ app.post("/login", function(req, res){
       console.log(err);
     } else {
       if (foundUser){
-        if (foundUser.password === password){
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result){
+          if (result == true){
+            res.render("secrets");
+          }
+        });
+
+
       }
     }
   });
